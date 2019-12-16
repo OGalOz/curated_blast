fee51f4c38b87e5e57461ccab537d25148c69981
Omree Gal-Oz <ogaloz@lbl.gov>
2019-12-16 11:46:54 -0800

    More info on display

--- set_env_var.sh
+++ set_env_var.sh
@@ -6,3 +6,5 @@ tst=$PWD/test/curated_blast_server_test.py
 ui_dir=$PWD/ui/narrative/methods/run_curated_blast/
 tmp_dir=$PWD/test_local/workdir/tmp/
 mymod=$PWD/lib/curated_blast/altered_files/
+disp=$ui_dir/display.yaml
+spec=$ui_dir/spec.json
