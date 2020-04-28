94057f8664b47788ac3cfef09f43f4d7ccc44911
Omree Gal-Oz <ogaloz@lbl.gov>
2020-04-27 18:19:32 -0700

    sumin access

--- set_env_vars.sh
+++ set_env_vars.sh
@@ -3,8 +3,8 @@
 chome=$PWD
 impl=$PWD/lib/curated_blast/curated_blastImpl.py
 tst=$PWD/test/curated_blast_server_test.py
-ui_dir=$PWD/ui/narrative/methods/run_curated_blast/
-tmp_dir=$PWD/test_local/workdir/tmp/
 mymod=$PWD/lib/curated_blast/altered_files/
-disp=$ui_dir/display.yaml
-spec=$ui_dir/spec.json
+tmp_dir=$PWD/test_local/workdir/tmp/
+ui_dir=$PWD/ui/narrative/methods/run_curated_blast/
+udisp=$ui_dir/display.yaml
+uspec=$ui_dir/spec.json
